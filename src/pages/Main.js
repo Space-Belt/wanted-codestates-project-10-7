e9fc5ca3fea328ed332d2f490791801dbc099af0
Space-Belt <dngurwn@gmail.com>
2022-03-07 21:35:41 +0900

    [Feat] #2 드래그앤드롭

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import DragnDrop from '../components/createForm/DragnDrop';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <DragnDrop />
+    </div>
+  );
 }
